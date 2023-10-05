5cfc6a847a2ec404df30aa1e73c5e32187f7c771
Medina Nesro <mediunique00@gmail.com>
2023-10-05 23:45:18 +0300

    Update 125. Valid Palindrome.py

--- 125. Valid Palindrome/125. Valid Palindrome.py
+++ 125. Valid Palindrome/125. Valid Palindrome.py
@@ -1,5 +1,7 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        lower=''.join( c.lower() for c in s if c.isalnum())
+        #used .join() function to join each letter as a string after checking the letter is alphanumeric and converting to lowercase using .lower() function
+        lower=''.join( char.lower() for char in s if char.isalnum())
         return lower==lower[::-1]
+        
         
