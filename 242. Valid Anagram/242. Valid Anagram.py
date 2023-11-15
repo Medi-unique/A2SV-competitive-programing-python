4381f23272b438034c3252cab4643839d6fef26f
Medina Nesro <mediunique00@gmail.com>
2023-11-16 02:47:50 +0300

    Update 242. Valid Anagram.py

--- 242. Valid Anagram/242. Valid Anagram.py
+++ 242. Valid Anagram/242. Valid Anagram.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def isAnagram(self,s: str,t: str) -> bool:
+        hm=defaultdict(str)
+        hm=defaultdict(str)
+        
+        if len(s) != len(t):
+            return False
+        hm1=sorted(s)
+        hm2=sorted(t)
+        if hm1==hm2:
+            return True
